337494f71940e115f2acef7651ba7463a47b9d84
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-12-11 23:56:49 +0630

    Coming Soon refactor

--- app/Http/Livewire/ComingSoon.php
+++ app/Http/Livewire/ComingSoon.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class ComingSoon extends Component
@@ -15,7 +16,7 @@ class ComingSoon extends Component
         $current = Carbon::now()->timestamp;
         $afterFourMonths = Carbon::now()->addMonths(4)->timestamp;
 
-        $this->comingSoon = Http::withHeaders(config('services.igdb'))
+        $comingSoonUnformatted = Http::withHeaders(config('services.igdb'))
             ->withBody("
                 fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count, summary;
                 where platforms = (48, 49, 130, 6)
@@ -27,9 +28,20 @@ class ComingSoon extends Component
             ->post('https://api.igdb.com/v4/games')
             ->json();
 
+        $this->comingSoon = $this->formatForView($comingSoonUnformatted);
     }
     public function render()
     {
         return view('livewire.coming-soon');
+    }
+
+    private function formatForView(array $comingSoonUnformatted): array
+    {
+        return collect($comingSoonUnformatted)->map(function ($game) {
+            return collect($game)->merge([
+                'coverImageUrl' => isset($game['cover']) ? Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) : null,
+                'first_release_date' =>  Carbon::parse($game['first_release_date'])->format('M d, Y')
+            ]);
+        })->toArray();
     }
 }
--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -3,12 +3,12 @@
         <div class="game flex">
             <a href="#">
             </a>
-            <img src="{{ Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']) }}" alt="game cover"
+            <img src="{{ $game['coverImageUrl'] }}" alt="game cover"
                  class="w-16 hover:opacity-75 transition ease-in-out duration-150"/>
             <div class="ml-4">
                 <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
                 <div class="text-gray-400 text-sm mt-1">
-                    {{ \Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
+                    {{ $game['first_release_date'] }}
                 </div>
             </div>
         </div>
